d9e309ee8ae4a8e62b80bc941662d641a73c3692
Adrian (adiomi games) <adiomigames@gmail.com>
2023-04-22 01:54:44 +0200

    Finish work on user panel page
    What's new?
    -fixed bugs on login page
    -added new functionality in user panel page: load posts

--- eWads/ViewModels/UserPanelViewModel.cs
+++ eWads/ViewModels/UserPanelViewModel.cs
@@ -1,7 +1,66 @@
-﻿namespace eWads.ViewModels
-{
-    public class UserPanelViewModel
-    {
+﻿using Caliburn.Micro;
+using eWadsLib;
+using eWadsLib.Structs;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
+namespace eWads.ViewModels
+{
+    public class UserPanelViewModel : Conductor<object>
+    {
+        public ObservableCollection<object> Posts { get; set; } = new ObservableCollection<object>();
+
+        public async void RefreshPosts()
+        {
+            var getPosts = await PostService.LoadAllPosts();
+            foreach (var post in getPosts)
+            {
+                Posts.Add(CreatePost(post));
+            }
+        }
+
+        private StackPanel CreatePost(PostData post)
+        {
+            StackPanel panel = new StackPanel();
+
+            TextBlock titleBlock = new TextBlock()
+            {
+                Text = post.Title,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                FontSize = 25
+            };
+
+            Image image = new Image()
+            {
+                Source = new BitmapImage(post.UrlImage)
+            };
+
+            StackPanel bgDescription = new StackPanel()
+            {
+                Background = new SolidColorBrush(Colors.CornflowerBlue),
+                Margin = new Thickness(20)
+            };
+
+            TextBlock descriptionBlock = new TextBlock()
+            {
+                Text = post.Description,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                FontSize = 20,
+                Foreground = new SolidColorBrush(Colors.White)
+            };
+            bgDescription.Children.Add(descriptionBlock);
+
+            panel.Children.Add(titleBlock);
+            panel.Children.Add(image);
+            panel.Children.Add(bgDescription);
+
+            return panel;
+        }
     }
 }
